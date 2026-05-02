7f0ff913034dee2cb47234154275d5d2c58f6614
Mahyar McDonald <22130+theontho@users.noreply.github.com>
2026-05-02 13:33:26 -0700

    chore: transition to pixi-driven dependencies and cleanup legacy scripts

--- dep_scripts/uninstall_fedora.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Uninstall dependencies for Fedora/RHEL/CentOS
-
-set -e
-
-echo "Uninstalling dependencies..."
-dnf remove -y samtools bcftools htslib bwa minimap2 fastp fastqc delly freebayes openjdk python3 python3-pip
-
-echo "Uninstallation complete."
